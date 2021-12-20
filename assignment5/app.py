6a1b5fedc18296b38529d2536253d2e03afb9bd9
tsilagava <tea.silagava@gmail.com>
2021-12-21 00:26:11 +0400

    created 3 route by using flask

--- assignment5/app.py
+++ assignment5/app.py
@@ -1,12 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def welcome():
+    return render_template("base.html")
 
+
+@app.route('/home')
+def home():
+    return render_template("home.html")
+
+
+@app.route('/about')
+def about():
+    return render_template("About.html")
 
 if __name__ == '__main__':
     app.run()
